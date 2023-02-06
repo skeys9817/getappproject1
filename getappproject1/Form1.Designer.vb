b7f578d45a36c39f30ea2ae8cca88e12acba4fed
skeys9817 <skeys9817@W10-CG51NL3>
2023-02-06 13:46:23 -0600

    Add project files.

---
+++ getappproject1/Form1.Designer.vb
@@ -0,0 +1,67 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.ListBox1 = New System.Windows.Forms.ListBox()
+        Me.bb = New System.Windows.Forms.PictureBox()
+        CType(Me.bb, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'ListBox1
+        '
+        Me.ListBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 25
+        Me.ListBox1.Items.AddRange(New Object() {"banana", "cookies and creme", "vanilla", "pistachio", "rocky road"})
+        Me.ListBox1.Location = New System.Drawing.Point(127, 24)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(202, 129)
+        Me.ListBox1.TabIndex = 0
+        '
+        'bb
+        '
+        Me.bb.Image = Global.getappproject1.My.Resources.Resources.banana
+        Me.bb.Location = New System.Drawing.Point(442, 93)
+        Me.bb.Name = "bb"
+        Me.bb.Size = New System.Drawing.Size(197, 185)
+        Me.bb.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.bb.TabIndex = 1
+        Me.bb.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(800, 450)
+        Me.Controls.Add(Me.bb)
+        Me.Controls.Add(Me.ListBox1)
+        Me.Name = "Form1"
+        Me.Text = "Form1"
+        CType(Me.bb, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents ListBox1 As ListBox
+    Friend WithEvents bb As PictureBox
+End Class
---
+++ getappproject1/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+    Private Sub ListBox1_SelectedIndexChanged(sender As Object, e As EventArgs) Handles ListBox1.SelectedIndexChanged
+        If ListBox1.SelectedIndex = 0 Then
+            bb.Image = My.Resources.banana
+        End If
+
+        If ListBox1.SelectedItem = "cookies and creme" Then
+            bb.Image = My.Resources.cookies_n_cream
+        End If
+
+        If ListBox1.SelectedItem = "vanilla" Then
+            bb.Image = My.Resources.vanilla
+        End If
+
+        If ListBox1.SelectedItem = "pistachio" Then
+            bb.Image = My.Resources.pistachio
+        End If
+
+        If ListBox1.SelectedItem = "rocky road" Then
+            bb.Image = My.Resources.rocky_road
+        End If
+    End Sub
+End Class
